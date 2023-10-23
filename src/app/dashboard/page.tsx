5d49dc393789e67e9110691b9f775298a6eadea8
Maximiliano Vilariño <maxi.vilarino@gmail.com>
2023-10-24 00:36:02 +0200

    Modify dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>This is the dashboard page</p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
